01968a4562de153269803a19c3e28263a8241a56
ryan2625 <ryancfreas@gmail.com>
2025-04-12 21:29:39 +0100

    Adding logging file locations to local dir

--- server/ecosystem.config.cjs
+++ server/ecosystem.config.cjs
@@ -8,11 +8,15 @@ module.exports = {
     script: "npm",
     args: 'run start',
     cwd: "./",
+    error_file: "./logs/express_err.log",
+    out_file: "./logs/express_out.log"
   },
   {
     name: "FastAPI_Server",
     cwd: "./livestream",
     script: './activation.sh',
+    error_file: "../logs/fast_err.log",
+    out_file: "../logs/fast_out.log"
   }]
 };
 //pm2 logs https://pm2.keymetrics.io/docs/usage/quick-start/
