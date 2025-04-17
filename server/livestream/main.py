b5befe94d4bb3b28c0f9244eb3fc971b9363c417
ryan2625 <ryancfreas@gmail.com>
2025-04-17 20:23:37 +0100

    Updating fast API architecture

--- server/livestream/main.py
+++ server/livestream/main.py
@@ -1,5 +1,6 @@
 import io
 import asyncio
+import time
 from picamera2 import Picamera2
 from picamera2.encoders import MJPEGEncoder, Quality
 from picamera2.outputs import FileOutput
@@ -9,6 +10,9 @@ from threading import Condition
 from contextlib import asynccontextmanager
 # see:  https://datasheets.raspberrypi.com/camera/picamera2-manual.pdf
 
+max_streams = 2
+timeout = 300
+upTime = 0
 class StreamingOutput(io.BufferedIOBase):
     def __init__(self):
         super().__init__()
@@ -30,20 +34,19 @@ class JpegStream:
     def __init__(self):
         self.active = False
         self.connections = set()
+        self.timeStampMap = {}
         self.activeStreams = 0
-        self.maxStreams = 5
+        self.maxStreams = max_streams
         self.masterSocket = None
         self.picam2 = None
         self.task = None
 
     async def stream_jpeg(self):
         self.picam2 = Picamera2()
-        video_config = self.picam2.create_video_configuration(
-            main={"size": (1920, 1080)}
-        )
-        self.picam2.configure(video_config)
+        self.picam2.video_configuration.controls.FrameRate = 15.0
+        self.picam2.video_configuration.size = (1920, 1080)
         output = StreamingOutput()
-        self.picam2.start_recording(MJPEGEncoder(), FileOutput(output), Quality.VERY_HIGH)
+        self.picam2.start_recording(MJPEGEncoder(), FileOutput(output), Quality.MEDIUM)
 
         try:
             while self.active:
@@ -52,6 +55,12 @@ class JpegStream:
                     websocket.send_bytes(jpeg_data)
                     for websocket in self.connections.copy()
                 ]
+                if (self.picam2.frames % 100 == 0):
+                    msgTasks = [
+                        websocket.send_text(f"Frames reached: {self.picam2.frames}")
+                        for websocket in self.connections.copy()
+                    ]
+                    await asyncio.gather(*msgTasks, return_exceptions=True)
                 await asyncio.gather(*tasks, return_exceptions=True)
         finally:
             self.picam2.stop_recording()
@@ -85,7 +94,9 @@ app.mount("/static", StaticFiles(directory='static'), name='static')
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
+    # jpeg_stream.connections.add({"websocket": websocket, "timeStamp": time.time()})
     jpeg_stream.connections.add(websocket)
+    jpeg_stream.timeStampMap[websocket] = time.time()
     try:
         while True:
             await websocket.receive_text()
@@ -101,7 +112,6 @@ async def start_stream():
     await jpeg_stream.start()
     return {"message": "Stream started"}
 
-
 @app.post("/stop")
 async def stop_stream():
     await jpeg_stream.stop()
@@ -114,3 +124,7 @@ async def send_status():
 @app.get("/connections")
 async def send_connections():
     return {'connections': len(jpeg_stream.connections)}
+
+@app.get("/frames")
+async def send_connections():
+    return {'connections': jpeg_stream.picam2.frames}
