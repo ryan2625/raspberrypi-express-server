ac72c751bb3b85a94191072b2573d50d7ea3739e
Ryan <ryancfreas@gmail.com>
2025-05-14 21:15:12 -0500

    Updating client side base files

---
+++ client/iot-cat/src/components/hero/Hero.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './hero.scss'
+
+function Hero() {
+  return (
+    <section className='hero-section'>
+      <nav>
+        Navbar
+      </nav>
+      <div className="hero-banner">
+
+      </div>
+    </section>
+  )
+}
+
+export default Hero
